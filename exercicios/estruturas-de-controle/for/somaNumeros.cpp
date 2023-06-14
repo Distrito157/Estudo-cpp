ec63b2a1a37bb1928fdb9f93a9d977e56a019613
Felipi Soares <felipidst@gmail.com>
2023-06-14 13:46:54 -0300

    adicionado exercicio de soma e tabuada do loop for

---
+++ exercicios/estruturas-de-controle/for/somaNumeros.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int soma = 0;
+  for (int i = 1; i <= 100; i++) {
+    soma += i;
+  }
+  cout << "A soma dos números de 1 a 100 é: " << soma << endl;
+  return 0;
+}
